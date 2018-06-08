c60d7c52696214da7d5ca983b63d40582f2be6fe
Ayodele Opetumo <aopetumo@gmail.com>
2018-06-08 09:43:52 +0100

    Add comments to different type of component

--- src/app/passenger-dashboard/passenger-dashboard.module.ts
+++ src/app/passenger-dashboard/passenger-dashboard.module.ts
@@ -4,12 +4,12 @@ import { HttpModule } from '@angular/http';
 import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 
-// Comtainers
+// Comtainers aka smart components
 import { PassengerViewerComponent } from './containers/passenger-viewer/passenger-viewer.component';
 import { PassengerDashboardComponent } from './containers/passenger-dashboard/passenger-dashboard.component';
 import { PassengerFormComponent } from './components/passenger-form/passenger-form.component';
 
-// Components
+// Components aka dumb/presentation components
 import { PassengerCountComponent } from './components/passenger-count/passenger-count.component';
 import { PassengerDetailComponent } from './components/passenger-detail/passenger-detail.component';
 
